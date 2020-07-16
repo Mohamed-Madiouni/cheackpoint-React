bd3b051cea8e9ebac7b19246880d9a6fed117245
Mohamed <madiounimohamed92@gmail.com>
2020-07-16 11:53:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comp from './Comp'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <form className="form">
+        <div className="form-group">
+            <label for="username">User Name</label>
+            <input type="text" className="form-control" id="username" placeholder="User name"/>
+          </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email Address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address"/>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
